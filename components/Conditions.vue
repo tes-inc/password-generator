b8973d0518cc994693f4ed5a84b692a629d17bf3
hayato-yamashita <h-yamashita@tes.co.jp>
2018-10-25 15:54:48 +0900

    数字と記号の合わせた桁数が、ユーザの指定した桁数よりも多くなった場合に期待しない動作になる不具合を修正

--- components/Conditions.vue
+++ components/Conditions.vue
@@ -2,19 +2,19 @@
   v-card
     v-subheader 生成条件
     v-card-text
-      v-form
+      v-form(ref="form" v-model="valid" lazy-validation)
         v-radio-group(v-model="strength" row)
           v-radio(label="普通" value="normal")
           v-radio(label="強い" value="strong")
           v-radio(label="カスタム" value="custom")
 
-        slider(label="桁数" v-model="total" :min="4" :max="64" :disabled="!isCustom")
-        slider(label="数字の数" v-model="digits" :min="0" :max="10" :disabled="!isCustom")
-        slider(label="記号の数" v-model="symbols" :min="0" :max="10" :disabled="!isCustom")
-        slider(label="生成する数" v-model="generates" :min="1" :max="50")
+        slider(label="桁数" v-model="total" min="4" max="64" :disabled="!isCustom" :rules="rules.total")
+        slider(label="数字の数" v-model="digits" min="0" max="10" :disabled="!isCustom")
+        slider(label="記号の数" v-model="symbols" min="0" max="10" :disabled="!isCustom")
+        slider(label="生成する数" v-model="generates" min="1" max="50")
 
     v-card-actions
-      v-btn(:loading="generating" :disabled="generating" block color="primary" @click="submit") 生成する
+      v-btn(:loading="generating" :disabled="!valid || generating" block color="primary" @click="submit") 生成する
 </template>
 
 <script>
@@ -24,24 +24,43 @@ export default {
   components: {
     Slider
   },
-  data: () => ({
-    strength: null,
-    total: 4,
-    digits: 0,
-    symbols: 0,
-    generates: 5
-  }),
+  data() {
+    return {
+      valid: true,
+      strength: null,
+      total: 4,
+      digits: 0,
+      symbols: 0,
+      generates: 5,
+      rules: {
+        total: [
+          v =>
+            v >= this.dynamicTotalMin ||
+            '数字と記号を合わせた桁数を下回っています'
+        ]
+      }
+    }
+  },
   computed: {
     generating() {
       return this.$store.state.generating
     },
     isCustom() {
       return this.strength === 'custom'
+    },
+    dynamicTotalMin() {
+      return this.digits + this.symbols
     }
   },
   watch: {
     strength() {
       this.setPreset()
+    },
+    digits() {
+      this.adjustLength()
+    },
+    symbols() {
+      this.adjustLength()
     }
   },
   mounted: function() {
@@ -60,6 +79,12 @@ export default {
           this.digits = 5
           this.symbols = 5
           break
+      }
+    },
+    adjustLength() {
+      // 数字・記号の桁数が変わったら、合計の桁数の帳尻を合わせる
+      if (this.total < this.dynamicTotalMin) {
+        this.total = this.dynamicTotalMin
       }
     },
     async submit() {
